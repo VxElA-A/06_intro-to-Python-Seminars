a947da4f34d353dec3af4448bc6f35d35b09ad8d
VxElA-A <aaleksandrenko@gmail.com>
2023-06-01 00:16:53 +0300

    Seminar 2 commit (Task 09-15)

---
+++ Task27/Task27.py
@@ -0,0 +1,25 @@
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+
+text = input('Введите текст: ').split()
+# for i in text:
+#     if i not in sl:
+print(text)
+
+
+# st = """She sells sea shells on the sea shore;The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore,I'm   sure    that the shells are sea
+# shore shells."""
+# stroka = st.replace(".", " ").replace(",", " ").replace("'", " ").replace(";", " ").lower()
+# spisok = st.split()
+# mnozestvo = set(spisok)
+# print(len(mnozestvo))
